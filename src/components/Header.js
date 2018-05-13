d3995d79aa8ef15dfba998ddaf7c04513f83e8b8
silva <htsilvakt04@gmail.com>
2018-05-14 01:08:09 +0700

    modify waord

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => (
     <div className="text-center blog-header">
         <h1 className="blog-title">The Trusting Social VietNam Time</h1>
-        <p className="lead blog-description">The official articles come from NYT (craft by API)</p>
+        <p className="lead blog-description">The official articles come from NYT (crafted by API)</p>
     </div>
 );
 
